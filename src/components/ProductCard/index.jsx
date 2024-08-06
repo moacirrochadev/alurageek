3d776cdb9b920d2eff50f1b04fe1c11062ae4230
Moacir Rocha <empreendedormoacirneto@gmail.com>
2024-08-06 11:40:08 -0300

    corrigindo as imagens

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -6,7 +6,7 @@ export function ProductCard(){
         <div className="w-48 h-64 flex flex-col text-zinc-50 border-4 border-zinc-950 rounded-lg p-2.5 bg-indigo-700 space-y-2">
           <img
             className="w-44"
-            src="src/assets/unsplash_6FDXGY9J6y4.svg"
+            src="./src/assets/unsplash_6FDXGY9J6y4.svg"
             alt="Imagem 1"
           />
           <p className="text-md text-left font-mono">Stormtrooper</p>
@@ -19,7 +19,7 @@ export function ProductCard(){
         </div>
 
         <div className="w-48 h-64 flex flex-col text-zinc-50 border-4 border-zinc-950 rounded-lg p-2.5 bg-indigo-700 space-y-2">
-          <img className="w-44" src="src/assets/image 2.svg" alt="Imagem 1" />
+          <img className="w-44" src="./src/assets/image 2.svg" alt="Imagem 1" />
           <p className="text-md text-left font-mono">Game Boy Classic</p>
           <div className="flex items-center justify-between">
             <p className="flex items-center gap-1 text-sm">

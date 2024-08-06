6bc271ff14aa9d306d037dcb0b96e410b2aee358
Moacir Rocha <empreendedormoacirneto@gmail.com>
2024-08-06 15:05:23 +0000

    ...

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ export function Header(){
     return (
       <header className="bg-indigo-700 font-sans text-2xl text-zinc-50 flex items-center justify-center gap-2 py-5">
         <div className="flex">
-          <img src="src/assets/image 1.png" alt="" />
+          <img src="./src/assets/image 1.png" alt="3 corações" />
         </div>
         <h1>AluraGeek</h1>
       </header>
